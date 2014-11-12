188acb17ed106356b7b82819dc1dd28c8a823d67
Kyle Morse <kmorse@rallydev.com>
2014-11-12 10:13:23 -0700

    Finalize context example

--- context/solution/App.js
+++ context/solution/App.js
@@ -9,7 +9,26 @@ Ext.define('Rally.gettingstarted.Context', {
      * The app execution entry point
      */
     launch: function() {
+        var context = this.getContext();
 
+        //Display the current workspace name
+
+        //Display the current project name
+
+        //Display the current user's name
+
+        //Display the current subscription something
+
+        //Display the current user's profile something
+
+        //Display the current workspace's datetime format
+
+        //Display the current user's permission level
+        //Sub Admin? Workspace Admin? Project Admin? Project Editor? Project Viewer?
+
+        //Is the global project scoping the same as this context's project scoping?
+
+        //Is there a timebox scope specified (iteration or release)?
     },
 
     /**
